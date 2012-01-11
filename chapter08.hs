6945d78e8fd853ad647fae7a6eca3cf08585d723
Tomoya Amachi <simpleplay1217@gmail.com>
2012-01-11 19:40:27 +0900

    finish ch.8 practice

--- chapter08.hs
+++ chapter08.hs
@@ -95,5 +95,73 @@ comment = do string "--"
 -- 最終的にはすべてnatの要素になるので、4倍以上効率の差がある
 
 --6
+token :: Parser a -> Parser a
+token p = do space
+             v <- p
+             space
+             return v
+
+natural :: Parser Int
+natural = token nat
+
+symbol :: String -> Parser String
+symbol xs = token (string xs)
+
+expr :: Parser Int
+
+
+expr = do t <- term
+          do symbol "+"
+             e <- expr
+             return (t + e)
+           +++ do symbol "-"
+                  e <- expr
+                  return (t - e)
+           +++ return t
+
+-- term :: Parser Int
+-- term = do f <- factor
+--           do symbol "*"
+--              t <- term
+--              return (f * t)
+--            +++ do symbol "/"
+--                   t <- term
+--                   return (f `div` t)
+--            +++ return f
+
+factor :: Parser Int
+factor = do symbol "("
+            e <- expr
+            symbol ")"
+            return e
+          +++ natural
+eval :: String -> Int
+eval xs = case parse expr xs of
+  [(n,[])] -> n
+  [(_,out)] -> error("unused input" ++ out)
+  [] -> error "invalid"
 
 --7
+
+term :: Parser Int
+term = do r <- root
+          do symbol "*"
+             t <- term
+             return (r * t)
+           +++ do symbol "/"
+                  t <- term
+                  return (r `div` t)
+           +++ return r
+
+root :: Parser Int
+root = do f <- factor
+          do symbol "^"
+             r <- root
+             return (f ^ r)
+           +++ return f
+
+--8
+-- a
+-- b
+-- c
+-- d
